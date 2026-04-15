9ba46edbc3f22daaf9d97f6510aab5a67ce04c27
Filipe Ramos <carlosfilipesoaresramos2001@gmail.com>
2026-04-15 16:21:29 -0300

    docs: add JSDoc comments to StyleSheet API

--- src/api/StyleSheet.ts
+++ src/api/StyleSheet.ts
@@ -1,11 +1,33 @@
-import type { BaseStyle, NestedStyles } from "../types/styles";
-import { StyleSheet as RNStyleSheet } from "react-native";
+import type { BaseStyle, NestedStyles } from '../types/styles';
+import { StyleSheet as RNStyleSheet } from 'react-native';
 
 export namespace StyleSheet {
   type NamedStyles<T> = { [P in keyof T]: BaseStyle | NestedStyles };
 
+  /** An identity function for creating style sheets. */
   export function create<T extends NamedStyles<T>>(
-    styles: T & NamedStyles<any>,
+    // The extra & NamedStyles<any> here helps Typescript catch typos: e.g.,
+    // the following code would not error with `styles: T | NamedStyles<T>`,
+    // but would error with `styles: T & NamedStyles<any>`
+    //
+    /**
+     * ```ts
+    StyleSheet.create({
+    someComponent: { marginLeft: 1, magrinRight: 1 },
+      });```
+     */
+    /**
+     * The extra `& NamedStyles<any>` here helps TypeScript catch typos: e.g.,
+     * the following code would not error with `styles: T | NamedStyles<T>`
+     * but would error with `styles: T & NamedStyles<any>`:
+     *
+     * ```ts
+     * StyleSheet.create({
+     *  someComponent: { marginLeft: 1, magrinRight: 1 },
+     * });
+     * ```
+     */
+    styles: T & NamedStyles<any>
   ): T {
     return styles;
   }
